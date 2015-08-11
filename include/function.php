d3a4a739003d9212bd8a255d4652a15cc726c533
Bryan Haberberger <bhaberbe@slu.edu>
2015-08-11 11:43:59 -0500

    merge

--- include/function.php
+++ include/function.php
@@ -45,8 +45,10 @@ function queryResult($field, $index, $term, $address, $exact, $offset, $limit) {
         }
     
         //and the ordering variable
-        $query5 = $query5 . " ORDER BY $column" . $pagination_part;
+        //$query5 = $query5 . " ORDER BY $column" . $pagination_part;
         //echo $query5;
+        $query5 = $query5 . " ORDER BY $column";
+
         
         // the full search string applied
         $query5result = mysqli_query($link, $query5.$pagination_part);
