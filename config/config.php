f806c6209a3a4272d9a5ee3e3eefdee03d9d639b
Bryan Haberberger <bhaberbe@slu.edu>
2015-10-02 09:38:59 -0500

    config fix

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+// Database constants
+// # Database Name
+// $db_name = "postgres";
+// # Database
+// $db_host = "localhost";
+// # DB port
+// $db_port = "5432";
+// # Database User
+// $db_user = "postgres";
+// 
+// # Database password
+// $db_pswd = "123psqluser";
+
+
+
+# Connection
+    //$link = mysqli_connect('localhost:3306', 'digisig', '1EMeeIIINnn', 'digisigres');
+    $link = mysqli_connect('localhost:3306', 'root', '1229@Oxford', 'digisigres');
+    if(!$link){
+        die('Could not connect: ' . mysqli_error($link));
+    }
+
+    ?>
